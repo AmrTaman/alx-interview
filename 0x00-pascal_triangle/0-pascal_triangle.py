01020dd4970a1775ecb5f896e6ca9480e019e9f9
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-10-07 03:10:17 +0300

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+def pascal_triangle(n):
+    """
+    iam here
+    """
+    top = [1]
+    bottom = []
+    if n <= 0:
+        return []
+    print(top)
+    while(n):
+        bottom.append(1)
+        if len(top) > 1:
+            for idx in range(len(top) - 1):
+                bottom.append(top[idx] + top[idx + 1])
+        bottom.append(1)
+        print("[{}]".format(",".join([str(x) for x in bottom])))
+        top = bottom
+        bottom = []
+        n -= 1
