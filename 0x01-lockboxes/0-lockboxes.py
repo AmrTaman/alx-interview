640acd72367342c74af0efe2e3fdc8a2749714d0
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-10-11 02:08:02 +0300

    Create 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this is module
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    iam here
+    """
+    unlocked = [False] * len(boxes)
+    queue = [0]
+
+    while queue:
+        target_box = queue.pop(0)
+        for key in boxes[target_box]:
+            if key < len(boxes) and not unlocked[key]:
+                unlocked[key] = True
+                queue.append(key)
+    return all(unlocked)
